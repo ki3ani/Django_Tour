72fd497abfb10ed8c9d1cb11f0e1e6c61bfec879
Kenneth Kimani <kimanim68@gmail.com>
2022-08-12 13:38:32 +0300

    adding sessions diez

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,37 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
 
-tasks = ["soccer","cycling","coding","gaming","school","work"]
+
+
+
+class AddTask(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value = 1, max_value = 15)
+
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
     
 def add(request):
-    return render(request, "tasks/add.html", )
+    if request.method == "POST":
+        form = AddTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect("/tasks/")
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+                })
+
+    return render(request, "tasks/add.html", {
+        "form":AddTask()
+    })
